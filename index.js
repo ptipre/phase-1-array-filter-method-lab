ff073e5dcfd79878659803a38d17edd6f4d01973
Prajay Tipre <prajaytipre@gmail.com>
2021-11-08 17:26:32 -0500

    commit tree 9067430faa1004636417e140e079f2e6b8fba3c7

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(array, name) {
+    return array.filter(driver => driver.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(array, letter) {
+    return array.filter(driver => driver.slice(0, letter.length) === letter)
+}
+
+function matchName(array, match) {
+    return array.filter(driver => driver.name === match);
+}
